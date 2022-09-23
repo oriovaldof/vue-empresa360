1ebc024b07e3dde41891b44f180f021f7dcd5c6a
Oriovaldo Fialho <oriovaldof@gmail.com>
2022-09-22 22:52:20 -0400

    incluindo bootstrap e cirando componente site

--- src/router.js
+++ src/router.js
@@ -2,8 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 //createWebHashHistory
 import Home from '@/views/Home.vue';
 import Login from '@/views/Login.vue';
+import Site from '@/views/Site.vue';
 
 const routes = [
+    {
+        path:'/',
+        component: Site
+    },
     {
         path:'/home',
         component: Home
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <h3>Home</h3>
-        <router-link to="/login">Navegar para "/login"</router-link>
+        <!-- <router-link to="/login">Navegar para "/login"</router-link> -->
+        <router-link to="/">Sair</router-link>
     </div>
 </template>
