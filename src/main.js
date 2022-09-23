a71eb05e1bb67c94d9877667732519b5c285bf3e
Oriovaldo Fialho <oriovaldof@gmail.com>
2022-09-22 22:03:17 -0400

    configurando router vue

--- src/main.js
+++ src/main.js
@@ -1,4 +1,29 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+//---inicio rotas
+import { createRouter, createWebHashHistory } from 'vue-router';
+import Home from '@/views/Home.vue';
+import Login from '@/views/Login.vue';
+
+const routes = [
+    {
+        path:'/home',
+        component: Home
+    },
+    {
+        path:'/login',
+        component: Login
+    }
+];
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes: routes
+});
+
+//---fim rotas
+
+const Vue = createApp(App);
+Vue.use(router); //adicionando as configurações de rotas a instancia do Vue
+Vue.mount('#app');
