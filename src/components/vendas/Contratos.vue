fb47596bfea9d01a92ec5cce126549d9f89e25ff
Oriovaldo Fialho <oriovaldof@gmail.com>
2022-09-28 08:19:45 -0400

    query params vue

--- src/components/vendas/Contratos.vue
+++ src/components/vendas/Contratos.vue
@@ -1,6 +1,15 @@
 <template>
     <div>
       <h5>Contratos</h5>
+      <div class="d-flex justify-content-between">
+
+     
+      <router-link class="btn btn-primary" :to="{name:'contratos',query:{leadId_like:1}}">LeadId=1</router-link>
+      <router-link class="btn btn-primary" to="/home/vendas/contratos?servicoId_like=2">ServicoId=1</router-link>
+
+      <router-link class="btn btn-success" :to="{name:'contratos',query:{leadId_like:1, servicoId_like:2 }}">LeadId=1 e ServicoId=2</router-link>
+      <router-link class="btn btn-success" to="/home/vendas/contratos?servicoId_like=2&leadId_like=2">ServicoId=1</router-link>
+    </div>
       <table class="table table-hover">
           <thead>
               <tr>
@@ -29,9 +38,21 @@
     import ApiMixin from '@/mixins/ApiMixin';
   export default {
     name: "Contratos",
+    data: ()=>({
+        parametrosDeRelacionamento:'_expand=lead&_expand=servico'
+    }),
     mixins:[ApiMixin],
     created() {
-      this.getDadosApi("http://localhost:3000/contratos?_expand=lead&_expand=servico");
+      this.getDadosApi(`http://localhost:3000/contratos?${this.parametrosDeRelacionamento}`);
     },
+    beforeRouteUpdate(to, from, next){
+        
+        const queryParams = new URLSearchParams(to.query).toString();
+        const url = `http://localhost:3000/contratos?${this.parametrosDeRelacionamento}&${queryParams}`;
+        
+        this.getDadosApi(url);
+
+        next();
+    }
   };
   </script>
